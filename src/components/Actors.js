a4a608a2156273f8b44f5e24a15919b27e10216a
Xica Felix <xicasfelix@gmail.com>
2022-09-25 10:09:03 -0400

    commit tree 704324f699a2772908b4fa41e7b641d3cf135e7e

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorsList = actors.map((a)=>
+    <div key={a.name}>
+      <h3>{a.name}</h3>
+      <ul>
+        {a.movies.map((m)=>
+        <li key={m}>{m}</li>
+        )}
+      </ul>
+    </div>
+  )
+  return <div>
+    <h1>Actors Page</h1>
+    {actorsList}
+    </div>;
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorsList= directors.map((d)=>
+  <div key={d.name}>
+    <h3>{d.name}</h3>
+    <ul>
+      {d.movies.map((m)=>
+      <li key={Math.random()*10}>{m}</li>
+      )}
+    </ul>
+  </div>
+  )
+  return <div>
+    <h1>Directors Page</h1>
+    {directorsList}
+    </div>;
 }
 
 export default Directors;
